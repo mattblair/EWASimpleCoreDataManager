dbce398687521b21edaa1d449885d481a966124b
Matt Blair <elsewisemedia@gmail.com>
2014-03-19 09:35:34 -0700

    Added files for GeoJSON demo

---
+++ EWASimpleCoreDataManagerDemo/GeoDataManager.h
@@ -0,0 +1,13 @@
+//
+//  GeoDataManager.h
+//  EWASimpleCoreDataManagerDemo
+//
+//  Created by Matt Blair on 3/19/14.
+//  Copyright (c) 2014 Elsewise. All rights reserved.
+//
+
+#import "EWASimpleCoreDataManager.h"
+
+@interface GeoDataManager : EWASimpleCoreDataManager
+
+@end
---
+++ EWASimpleCoreDataManagerDemo/GeoDataManager.m
@@ -0,0 +1,13 @@
+//
+//  GeoDataManager.m
+//  EWASimpleCoreDataManagerDemo
+//
+//  Created by Matt Blair on 3/19/14.
+//  Copyright (c) 2014 Elsewise. All rights reserved.
+//
+
+#import "GeoDataManager.h"
+
+@implementation GeoDataManager
+
+@end
