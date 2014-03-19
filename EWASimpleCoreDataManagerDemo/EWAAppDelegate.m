37c35749ce91ea632a37a1c70f032bca12d80b78
Matt Blair <elsewisemedia@gmail.com>
2014-03-19 09:07:15 -0700

    Added startup database population to app delegate

--- EWASimpleCoreDataManagerDemo/EWAAppDelegate.m
+++ EWASimpleCoreDataManagerDemo/EWAAppDelegate.m
@@ -7,17 +7,29 @@
 //
 
 #import "EWAAppDelegate.h"
+#import "DemoDataManager.h"
 
 @implementation EWAAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    if ([[DemoDataManager sharedInstance] confirmDatabaseInstalled]) {
+        
+        NSLog(@"Datastore installed.");
+    } else {
+        NSLog(@"WARNING: DATASTORE NOT INSTALLED.");
+    }
+    
+    [[DemoDataManager sharedInstance] populateIfEmpty];
+    
     // Override point for customization after application launch.
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
         UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
         UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
         splitViewController.delegate = (id)navigationController.topViewController;
     }
+    
     return YES;
 }
 							
